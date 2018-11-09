8e794444a4f36aec7a211c0b2fe5b88503883175
v_liweiwei02 <v_liweiwei02@baidu.com>
2018-11-09 20:06:58 +0800

    完善异步处理reset接口实现

--- security-demo/src/main/java/fun/lww/securitydemo/web/AsyncOrderController.java
+++ security-demo/src/main/java/fun/lww/securitydemo/web/AsyncOrderController.java
@@ -2,6 +2,7 @@ package fun.lww.securitydemo.web;
 
 import fun.lww.securitydemo.async.DeferredResultHolder;
 import fun.lww.securitydemo.async.MockQueue;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 /**
@@ -24,6 +29,7 @@ public class AsyncOrderController {
     private Logger log = LoggerFactory.getLogger(getClass());
     //设置超时等待时间3000ms 超时返回timeout字符串
     private DeferredResult<String> deferredResult = new DeferredResult<>(3000L, "timeout");
+    private SseEmitter sseEmitter = new SseEmitter();
 
     @Autowired
     private MockQueue mockQueue;
@@ -31,8 +37,9 @@ public class AsyncOrderController {
     @Autowired
     private DeferredResultHolder deferredResultHolder;
 
+    //方式一 Callable
+    // ==================================================
     /**
-     * 方式一 Callable
      * <p>
      * 使用Callable接口 异步返回业务结果
      * 返回Callable对象时，实际工作线程会在后台处理，Controller无需等待工作线程处理完成，
@@ -61,8 +68,9 @@ public class AsyncOrderController {
         return result;
     }
 
+    // 方式二 DeferredResult
+    // ==================================================
     /**
-     * 方式二 DeferredResult
      * <p>
      * 特性：
      * 超时配置：通过构造函数可以传入超时时间，单位为毫秒；因为需要等待设置结果后才能继续处理并返回客户端，如果一直等待会导致客户端一直无响应，因此必须有相应的超时机制来避免这个问题；实际上就算不设置这个超时时间，应用服务器或者Spring也会有一些默认的超时机制来处理这个问题。
@@ -116,5 +124,58 @@ public class AsyncOrderController {
         return "success";
     }
 
+    // 方式三 SseEmitter
+    // ==================================================
+    /**
+     * 返回SseEmitter对象
+     *
+     * @return
+     */
+    @RequestMapping("/testSseEmitter")
+    public SseEmitter testSseEmitter() {
+        return sseEmitter;
+    }
+
+    /**
+     * 向SseEmitter对象发送数据
+     *
+     * @return
+     */
+    @RequestMapping("/setSseEmitter")
+    public String setSseEmitter() {
+        try {
+            sseEmitter.send(System.currentTimeMillis());
+        } catch (IOException e) {
+            log.error("IOException!", e);
+            return "error";
+        }
+        return "success";
+    }
+
+    /**
+     * 将SseEmitter对象设置成完成
+     *
+     * @return
+     */
+    @RequestMapping("/completeSseEmitter")
+    public String completeSseEmitter() {
+        sseEmitter.complete();
+        return "complete";
+    }
+
+    // 方式四 StreamingResponseBody
+    // ==================================================
+    /**
+     * 用于直接将结果写出到Response的OutputStream中； 如文件下载等
+     * @return
+     */
+    @GetMapping("/stream")
+    public StreamingResponseBody handle() {
+        return (outputStream) -> {
+            String path = "E:\\projects\\security\\security-demo\\src\\main\\resources\\file\\test.txt";
+            File file = new File(path);
+            outputStream.write(FileUtils.readFileToString(file, "UTF-8").getBytes());
+        };
+    }
 
 }
